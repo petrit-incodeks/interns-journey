b2be4aec69e4e8fa6aa92716933390ff038acc80
petrit-incodeks <105634679+petrit-incodeks@users.noreply.github.com>
2023-02-11 21:23:36 +0100

    Implement task02 :Create React component that displays a button (#9)
    Co-authored-by: FlorentZejna1 <florent.zejna@incodeks.com>

--- src/components/solutions/task02/Solution2.js
+++ src/components/solutions/task02/Solution2.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from "./Button";
 
 function Solution2(props) {
   return (
     <div>
       <h1>Solution for assignment #{props.assignmentId}</h1>
+      <Button />
     </div>
   );
 }
