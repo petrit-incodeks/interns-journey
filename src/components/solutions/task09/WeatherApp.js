50d027ce395ce5d23ed12cf0b5575e8bacae1ac7
FlorentZejna1 <florent.zejna@incodeks.com>
2023-02-28 11:45:12 +0100

    Add simple styling, Finish simple App that shows the Weather for input location

--- src/components/solutions/task09/WeatherApp.js
+++ src/components/solutions/task09/WeatherApp.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 
 function WeatherApp() {
-  //   const city = "Ferizaj";
   const key = "768455ddf8164cf48d984030232802";
 
   const [city, setCity] = useState("ferizaj");
@@ -19,7 +18,6 @@ function WeatherApp() {
     setUrl(
       `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`
     );
-
     setCity("");
   };
 
@@ -32,15 +30,14 @@ function WeatherApp() {
           city: response.location.name,
           country: response.location.country,
           temperatureOnC: response.current.temp_c,
-          temperatureOnF: response.current.temp_f,
           icon: response.current.condition.icon,
           text: response.current.condition.text,
         };
         setCurrentWeather(data);
-        // console.log(currentWeather);
-        // console.log(data.city, ":", data.temperatureOnC, "C");
-        // console.log(data);
       } catch (e) {
+        if (city !== currentWeather.city) {
+          alert("this city does not exist");
+        }
         console.log(e);
       }
     };
@@ -48,20 +45,28 @@ function WeatherApp() {
   }, [url]);
 
   return (
-    <div>
+    <div className="WeatherApp">
       <h1>Weather App</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter City name"
-          onChange={handleChange}
-        />
-        <button>Search</button>
-      </form>
-      <h2>{currentWeather.city}</h2>
-      <h3>{currentWeather.temperatureOnC} C</h3>
-      <img src={currentWeather.icon} alt=" weather icon" />
-      <p> {currentWeather.text}</p>
+      <div className="WeatherApp-form">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter City name"
+            onChange={handleChange}
+          />
+          <button className="WeatherApp-button">Search</button>
+        </form>
+      </div>
+      <div className="WeatherApp-info">
+        <h2>
+          {currentWeather.city}, {currentWeather.country}
+        </h2>
+        <div className="WeatherApp-info-card">
+          <h3>{currentWeather.temperatureOnC} °C</h3>
+          <img src={currentWeather.icon} alt=" weather icon" />
+          <p> {currentWeather.text}</p>
+        </div>
+      </div>
     </div>
   );
 }
