cc55bd88916e44d922eaf474b9ea5af60e7a879d
FlorentZejna1 <florent.zejna@incodeks.com>
2023-02-28 11:13:46 +0100

    Render simple information about weather in the requested city

--- src/components/solutions/task09/WeatherApp.js
+++ src/components/solutions/task09/WeatherApp.js
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from "react";
 
 function WeatherApp() {
-  const city = "Ferizaj";
+  //   const city = "Ferizaj";
   const key = "768455ddf8164cf48d984030232802";
 
-  //   const [currentWeather, setCurrentWeather] = useState({});
+  const [city, setCity] = useState("ferizaj");
+  const [currentWeather, setCurrentWeather] = useState({});
 
   const [url, setUrl] = useState(
     `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`
   );
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setUrl(
+      `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`
+    );
+
+    setCity("");
+  };
 
   useEffect(() => {
     const getWeather = async () => {
@@ -20,23 +33,35 @@ function WeatherApp() {
           country: response.location.country,
           temperatureOnC: response.current.temp_c,
           temperatureOnF: response.current.temp_f,
+          icon: response.current.condition.icon,
+          text: response.current.condition.text,
         };
-        console.log(data.city);
-        // console.log(response);
+        setCurrentWeather(data);
+        // console.log(currentWeather);
+        // console.log(data.city, ":", data.temperatureOnC, "C");
+        // console.log(data);
       } catch (e) {
-        alert(e);
+        console.log(e);
       }
     };
     getWeather();
   }, [url]);
 
-  const handleClick = () => {};
   return (
     <div>
-      <h1>Weather for :{city}</h1>
-
-      <input type="text" placeholder="Enter City name" />
-      <button onClick={handleClick}>Search</button>
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter City name"
+          onChange={handleChange}
+        />
+        <button>Search</button>
+      </form>
+      <h2>{currentWeather.city}</h2>
+      <h3>{currentWeather.temperatureOnC} C</h3>
+      <img src={currentWeather.icon} alt=" weather icon" />
+      <p> {currentWeather.text}</p>
     </div>
   );
 }
