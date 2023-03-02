752688b97559ccc4ba2bbf29d366cf812676f3e7
FlorentZejna1 <florent.zejna@incodeks.com>
2023-03-02 11:18:47 +0100

    Implement solution with async function

--- src/components/solutions/task14/RandQuoteApp.js
+++ src/components/solutions/task14/RandQuoteApp.js
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
-const quotes = [
-  "Life isn’t about getting and having, it’s about giving and being.",
-  "Whatever the mind of man can conceive and believe, it can achieve.",
-  "If you do what you’ve always done, you’ll get what you’ve always gotten.",
-  "You may be disappointed if you fail, but you are doomed if you don’t try.",
-  "If you do what you’ve always done, you’ll get what you’ve always gotten.",
-];
+
+const url =
+  "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 function RandQuoteApp() {
   const [quote, setQuote] = useState("");
-  function getQuote() {
-    let randQuote = Math.floor(Math.random() * quotes.length);
-    let randomQuote = quotes[randQuote];
-    setQuote(randomQuote);
+  const [author, setAuthor] = useState("");
+  async function getQuote() {
+    try {
+      let response = await fetch(url);
+      response = await response.json();
+
+      let rand = Math.floor(Math.random() * response.quotes.length);
+      let randomQuote = response.quotes[rand];
+      console.log(randomQuote);
+      setQuote(randomQuote.quote);
+      setAuthor(randomQuote.author);
+    } catch (error) {
+      console.log(error);
+    }
   }
+
   return (
     <div>
       <h2>Click To get a random Quote : </h2>
       <button onClick={getQuote}>New Quote</button>
-      <h3>{quote}</h3>
+      <h3>
+        {quote} <span>{author}</span>
+      </h3>
     </div>
   );
 }
