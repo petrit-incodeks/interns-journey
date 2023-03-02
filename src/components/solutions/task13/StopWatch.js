309f46e84548c49a42939b055234029fe6c60cb3
FlorentZejna1 <florent.zejna@incodeks.com>
2023-03-02 14:47:38 +0100

    added a coma between seconds and ms for a better look

--- src/components/solutions/task13/StopWatch.js
+++ src/components/solutions/task13/StopWatch.js
@@ -22,7 +22,7 @@ function StopWatch() {
 
       <h2>
         <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)},</span>
         <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
       </h2>
       <div>
