f40b118f5a1a94bbf7b3426122ae405437f81a04
FlorentZejna1 <florent.zejna@incodeks.com>
2023-03-03 16:14:19 +0100

    Render  multiple choice answers in the screen and add next button

--- src/components/solutions/task10/QuizApp.js
+++ src/components/solutions/task10/QuizApp.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { quiz } from "./questions";
 
 function QuizApp() {
-  // keep track of how many active questions are
+  // keep track of the  active question
   // keep track of the selected answer,
   // and keep track of the result in which we should track the num of correct answers,wrong answers,and the score of the player
 
@@ -13,12 +13,23 @@ function QuizApp() {
     correctAnswers: 0,
     wrongAnswers: 0,
   });
-  const { questions } = quiz;
 
+  //   function handleNext(){
+
+  //   }
+
+  const { questions } = quiz;
   return (
     <div>
       <h1>Quiz App</h1>
       <h3>Question: {questions[activeQuestion].question} </h3>
+      {questions[activeQuestion].choices.map((choice, i) => (
+        <button key={i}>{choice}</button>
+      ))}
+
+      <div>
+        <button onClick={handleNext}>Next</button>
+      </div>
     </div>
   );
 }
