275fe79c204bbf44246b48fccccc5a5ca70813a4
FlorentZejna1 <florent.zejna@incodeks.com>
2023-03-03 15:52:40 +0100

    Create QuizApp component, add a questions file , and render a question on the screen

---
+++ src/components/solutions/task10/QuizApp.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { quiz } from "./questions";
+
+function QuizApp() {
+  // keep track of how many active questions are
+  // keep track of the selected answer,
+  // and keep track of the result in which we should track the num of correct answers,wrong answers,and the score of the player
+
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+  const { questions } = quiz;
+
+  return (
+    <div>
+      <h1>Quiz App</h1>
+      <h3>Question: {questions[activeQuestion].question} </h3>
+    </div>
+  );
+}
+export default QuizApp;
---
+++ src/components/solutions/task10/questions.js
@@ -0,0 +1,15 @@
+export const quiz = {
+  topic: "general",
+  level: "Beginner",
+  totalQuestions: 4,
+  perQuestionScore: 2,
+
+  questions: [
+    {
+      question: "Which galaxy is the earth in?",
+      choices: ["milky way", "far away", "andromeda", "222 galaxy"],
+      type: "multiple choice Question",
+      correctAnswer: "milky way",
+    },
+  ],
+};
