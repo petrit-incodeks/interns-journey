f6f82ec075e67d82a520d0cfdfbadacacc569160
FlorentZejna1 <florent.zejna@incodeks.com>
2023-03-03 17:01:25 +0100

    Add selected answer logic

--- src/components/solutions/task10/QuizApp.js
+++ src/components/solutions/task10/QuizApp.js
@@ -7,12 +7,16 @@ function QuizApp() {
   // and keep track of the result in which we should track the num of correct answers,wrong answers,and the score of the player
 
   const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState("");
   const [result, setResult] = useState({
     score: 0,
     correctAnswers: 0,
     wrongAnswers: 0,
   });
+
+  const { questions } = quiz;
+  const { question, choices, correctAnswer } = questions[activeQuestion];
 
   function handleNext() {
     // alert("go to the next question!");
@@ -23,18 +27,35 @@ function QuizApp() {
     //if i am, replace the button next with finish quiz
   }
 
-  const { questions } = quiz;
+  function onSelectedAnswer(answer, id) {
+    setSelectedAnswerIndex(id);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+      console.log("correct");
+    } else {
+      setSelectedAnswer(false);
+      console.log("wrong");
+    }
+  }
+
   return (
     <div>
       <h1>Quiz App</h1>
       <h3>Question: {questions[activeQuestion].question} </h3>
-      {/* change the buttons to be list items */}
+      {/* change the buttons to be list items later*/}
       {questions[activeQuestion].choices.map((choice, i) => (
-        <button key={i}>{choice}</button>
+        <button onClick={onSelectedAnswer} key={i}>
+          {choice}
+        </button>
       ))}
 
       <div>
         <button onClick={handleNext}>Next</button>
+      </div>
+      <div>
+        <h3>Result: {result.score}</h3>
+        <h4>Correct Answers: {result.correctAnswers}</h4>
+        <h4>Wrong Answers: {result.wrongAnswers}</h4>
       </div>
     </div>
   );
