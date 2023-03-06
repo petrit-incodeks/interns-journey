57a516b23a7b9459a80c1e913ad307466d431d9f
FlorentZejna1 <florent.zejna@incodeks.com>
2023-03-06 09:00:18 +0100

    put the url inside the function, and remove console.log

--- src/components/solutions/task14/RandQuoteApp.js
+++ src/components/solutions/task14/RandQuoteApp.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 
-const url =
-  "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 function RandQuoteApp() {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
 
   function getQuote() {
     return new Promise((resolve, reject) => {
-      fetch(url)
+      fetch(
+        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+      )
         .then((response) => response.json())
         .then((response) => {
           let rand = Math.floor(Math.random() * response.quotes.length);
@@ -18,7 +18,6 @@ function RandQuoteApp() {
           resolve();
         })
         .catch((error) => {
-          console.log(error);
           reject(error);
         });
     });
