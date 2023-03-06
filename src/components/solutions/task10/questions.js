77fcee24cf1c9348495de06531e1b883a0183244
FlorentZejna1 <florent.zejna@incodeks.com>
2023-03-06 13:18:02 +0100

    Add more quiz questions

--- src/components/solutions/task10/questions.js
+++ src/components/solutions/task10/questions.js
@@ -1,7 +1,7 @@
 export const quiz = {
   topic: "general",
   level: "Beginner",
-  totalQuestions: 4,
+  totalQuestions: 6,
   perQuestionScore: 2,
 
   questions: [
@@ -16,6 +16,37 @@ export const quiz = {
       choices: ["Sirius", "Sun", "North Star", "22033-sd"],
       type: "multiple choice Question",
       correctAnswer: "Sun",
+    },
+    {
+      question: "How many minutes are in a full week?",
+      choices: ["10000", "10,080", "11000", "60"],
+      type: "multiple choice Question",
+      correctAnswer: "10,080",
+    },
+    {
+      question:
+        "What is the highest-rated film on IMDb as of January 1st, 2022?",
+      choices: [
+        "The Shawshank Redemption",
+        "American Pie",
+        "Avatar 2",
+        "Top Gun",
+      ],
+      type: "multiple choice Question",
+      correctAnswer: "The Shawshank Redemption",
+    },
+    {
+      question: "Which planet has the most moons?",
+      choices: ["Saturn", "Jupiter", "Earth", "Venus"],
+      type: "multiple choice Question",
+      correctAnswer: "Saturn",
+    },
+    {
+      question:
+        "What software company is headquartered in Redmond, Washington?",
+      choices: ["Oracle", "Apple", "Google", "Microsoft"],
+      type: "multiple choice Question",
+      correctAnswer: "Microsoft",
     },
   ],
 };
