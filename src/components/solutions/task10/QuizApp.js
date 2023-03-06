67ae43df31a2a6b915633f38e2e9851d4c116230
FlorentZejna1 <florent.zejna@incodeks.com>
2023-03-06 10:50:26 +0100

    complete QuizApp functionality and render results on screen

--- src/components/solutions/task10/QuizApp.js
+++ src/components/solutions/task10/QuizApp.js
@@ -9,6 +9,7 @@ function QuizApp() {
   const [activeQuestion, setActiveQuestion] = useState(0);
   const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [showResults, setShowResults] = useState(false);
   const [result, setResult] = useState({
     score: 0,
     correctAnswers: 0,
@@ -19,45 +20,69 @@ function QuizApp() {
   const { question, choices, correctAnswer } = questions[activeQuestion];
 
   function handleNext() {
-    // alert("go to the next question!");
+    setSelectedAnswerIndex(null);
+    setResult((prevRes) =>
+      selectedAnswer
+        ? {
+            ...prevRes,
+            score: prevRes.score + 2,
+            correctAnswers: prevRes.correctAnswers + 1,
+          }
+        : {
+            ...prevRes,
+            wrongAnswers: prevRes.wrongAnswers + 1,
+          }
+    );
 
-    setActiveQuestion((prevQuestion) => prevQuestion + 1);
-    //edge cases --
-    // i need to check  if i'm in the last question
-    //if i am, replace the button next with finish quiz
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((prevQuestion) => prevQuestion + 1);
+    } else {
+      setActiveQuestion(0);
+      setShowResults(true);
+    }
   }
 
-  function onSelectedAnswer(answer) {
-    if (answer === questions[activeQuestion].correctAnswer) {
+  function onSelectedAnswer(answer, i) {
+    setSelectedAnswerIndex(i);
+    if (answer === correctAnswer) {
       setSelectedAnswer(true);
-      console.log("correct");
     } else {
       setSelectedAnswer(false);
-      console.log("wrong");
     }
   }
 
   return (
-    <div>
+    <div className="Quiz">
       <h1>Quiz App</h1>
-      <h3>Question: {questions[activeQuestion].question} </h3>
-      {/* change the buttons to be list items later*/}
-      <ul>
-        {questions[activeQuestion].choices.map((choice, i) => (
-          <li onClick={() => onSelectedAnswer(choice)} key={i}>
-            {choice}
-          </li>
-        ))}
-      </ul>
+      {!showResults ? (
+        <div>
+          <h3>Question: {question} </h3>
+          <ul>
+            {choices.map((choice, i) => (
+              <li onClick={() => onSelectedAnswer(choice, i)} key={choice}>
+                {choice}
+              </li>
+            ))}
+          </ul>
 
-      <div>
-        <button onClick={handleNext}>Next</button>
-      </div>
-      <div>
-        <h3>Result: {result.score}</h3>
-        <h4>Correct Answers: {result.correctAnswers}</h4>
-        <h4>Wrong Answers: {result.wrongAnswers}</h4>
-      </div>
+          <div>
+            <button
+              onClick={handleNext}
+              disabled={selectedAnswerIndex === null}
+            >
+              {activeQuestion === questions.length - 1 ? "finish" : "next"}
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h3>Results</h3>
+          <h4>Total Questions : {questions.length}</h4>
+          <h4>Total Score:{result.score}</h4>
+          <h4>Correct Answers: {result.correctAnswers}</h4>
+          <h4>Wrong Answers: {result.wrongAnswers}</h4>
+        </div>
+      )}
     </div>
   );
 }
