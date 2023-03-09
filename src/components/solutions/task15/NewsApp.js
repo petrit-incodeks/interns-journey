4efed027dd15b0795c6c3c5f347e89c1b8b09a86
FlorentZejna1 <florent.zejna@incodeks.com>
2023-03-09 13:34:15 +0100

    fix bugs and finish functionality,style changes

--- src/components/solutions/task15/NewsApp.js
+++ src/components/solutions/task15/NewsApp.js
@@ -1,49 +1,66 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function NewsApp() {
-  const [state, setState] = useState("");
-  const [title, setTitle] = useState("");
-  const [desc, setDesc] = useState("");
-  const options = {
-    method: "GET",
-    headers: {
-      "X-BingApis-SDK": "true",
-      "X-RapidAPI-Key": "aac0c4e556msh8fcce3953d23e23p1bd9e3jsn772b0285dd9c",
-      "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
-    },
-  };
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-BingApis-SDK": "true",
+        "X-RapidAPI-Key": "a5a4a937admsh43206b5aeb29189p15126fjsnfe784eb0305e",
+        "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+      },
+    };
 
-  fetch(
-    "https://bing-news-search1.p.rapidapi.com/news?safeSearch=Off&textFormat=Raw",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-  // let rand = Math.floor(Math.random() * response.value.length);
-  //       // let randArticle = response.value[rand].image.thumbnail.contentUrl;
-  //       // let randArticleTitle = response.value[rand].name;
-  //       // let randArticleData = response.value[rand].description;
-  //       // setState(randArticle);
-  //       // setTitle(randArticleTitle);
-  //       // setDesc(randArticleData);
+    fetch(
+      "https://bing-news-search1.p.rapidapi.com/news/search?q=%3CREQUIRED%3E&freshness=Day&textFormat=Raw&safeSearch=Off",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setData(response.value))
+      .catch((err) => console.error(err));
+  }, []);
 
+  let arr = [];
+
+  for (let i = 0; i < data.length; i++) {
+    arr.push(data[i]);
+  }
+  // console.log(arr);
+  // // console.log(arr[0].image.thumbnail.contentUrl);
+  // arr.map((data) => {
+  //   console.log(data.image);
+  //   // console.log(data.image.thumbnail.contentUrl);
+  // });
+  // console.log(arr[0].image.thumbnail.contentUrl);
   return (
-    <div>
-      <h1>Latest News</h1>
-      <button>incodeks</button>
-
-      <article>
-        <h3>{title}</h3>
-        <img src={state} alt="articlepic" />
-        <p>{desc}</p>
-      </article>
-      <article>
-        <h3>Article 2</h3>
-      </article>
-      <article>
-        <h3>Article 3</h3>
-      </article>
+    <div className="NewsApp">
+      <div className="NewsApp_nav">
+        <h1>Latest News</h1>
+        <button>incodeks</button>
+      </div>
+      <div className="container">
+        <div className="row news_container">
+          {arr.map((data, i) => (
+            <div key={i} className="col-lg-3 news_card">
+              <div className="col-lg-12 news_card_img">
+                <img
+                  src={`${data.image.thumbnail.contentUrl}`}
+                  alt="articleImage"
+                />
+              </div>
+              <div className="col-lg-12 news_card_body">
+                <h5>{data.name}</h5>
+                <p>{data.description}</p>
+              </div>
+              <div className="col-lg-12 news_card_footer">
+                <small>{data.datePublished}</small>
+                <small>{data.datePublished}</small>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
