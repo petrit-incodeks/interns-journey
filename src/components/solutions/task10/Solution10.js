9d1d4d678550bd5247750d718e3214dbc29872ca
FlorentZejna1 <florent.zejna@incodeks.com>
2023-03-13 13:16:06 +0100

    Task10 stuff

--- src/components/solutions/task10/Solution10.js
+++ src/components/solutions/task10/Solution10.js
@@ -1,9 +1,11 @@
 import React from "react";
+import QuizApp from "./QuizApp";
 
 function Solution10(props) {
   return (
     <div>
       <h1>Solution for assignment #{props.assignmentId}</h1>
+      <QuizApp />
     </div>
   );
 }
